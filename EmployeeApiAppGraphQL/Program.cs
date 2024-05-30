7b94b1cd11c061b2f46122c7c9e1964a2dfda652
oussamaettaoui <120644277+oussamaettaoui@users.noreply.github.com>
2024-05-30 10:04:45 +0100

    Configure-Project

--- EmployeeApiAppGraphQL/Program.cs
+++ EmployeeApiAppGraphQL/Program.cs
@@ -9,14 +9,36 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"), sqlOptions => sqlOptions.EnableRetryOnFailure()), ServiceLifetime.Transient);
+//Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        corsBuilder => corsBuilder.WithOrigins("http://localhost:5173")
+                                  .AllowAnyHeader()
+                                  .AllowAnyMethod()
+                                  .AllowCredentials());
+});
+//
+builder.Services.AddGraphQL(options => {
+    options.EnableMetrics = false;
+}).AddSystemTextJson();
+// 
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultSQLConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure()
+    ), ServiceLifetime.Transient);
+//
 builder.Services.AddScoped<EmployeeRepository>();
 builder.Services.AddScoped<EmployeeQuery>();
 builder.Services.AddScoped<AppSchema>();
 builder.Services.AddScoped<EmployeeMutation>();
 builder.Services.AddControllers();
-// Register GraphQl
-builder.Services.AddGraphQL().AddSystemTextJson();
+// Add GraphQL
+builder.Services.AddGraphQL(options =>
+{
+    options.EnableMetrics = false;
+}).AddSystemTextJson();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,11 +51,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//GraphQl
+// Enable CORS with the specified policy
+app.UseCors("AllowSpecificOrigin");
+// Register GraphQL endpoints
 app.UseGraphQL<AppSchema>();
 app.UseGraphQLGraphiQL("/ui/graphql");
 app.UseHttpsRedirection();
-
+app.UseCors("AllowSpecificOrigin");
 app.UseAuthorization();
 
 app.MapControllers();
